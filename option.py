0710c859bbf8ab62a4bbf37fd63aee02c74f265f
Matt Brigida <mdbrigida@gmail.com>
2019-03-28 11:21:41 -0400

    added option_type

--- option.py
+++ option.py
@@ -1,15 +1,20 @@
 
 
 class Option:
-    def __init__(self, stock_price, vol, strike, time, risk_free, dividend_yield):
+    def __init__(self, stock_price, vol, strike_price, time, risk_free, dividend_yield, option_type):
         self.stock_price = stock_price
         self.vol = vol
-        self.strike = strike
+        self.strike_price = strike_price
         self.time = time
         self.risk_free = risk_free
         self.dividend_yield = dividend_yield
+        self.option_type = option_type
 
     def intrinsic(self):
-        return max(self.stock_price - self.strike, 0)
+        if self.option_type == "Call":
+            return max(self.stock_price - self.strike_price, 0)
+        else:
+            return max(self.strike_price - self.stock_price, 0)
 
-    def black_scholes_value(self)
+#    def black_scholes_value(self):
+        
