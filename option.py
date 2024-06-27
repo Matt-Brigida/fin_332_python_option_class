b7723bf5a4b7f5a38c95307e2c749bddd278f14a
Matt Brigida <mdbrigida@gmail.com>
2024-06-26 22:16:49 -0400

    d2 call

--- option.py
+++ option.py
@@ -1,4 +1,4 @@
-
+import math
 
 class Option:
     def __init__(self, stock_price, vol, strike_price, time, risk_free, dividend_yield, option_type):
@@ -9,6 +9,13 @@ class Option:
         self.risk_free = risk_free
         self.dividend_yield = dividend_yield
         self.option_type = option_type
+        if self.option_type == "Call":
+            self.d1 = (math.log(self.strike_price / self.strike_price) + (self.risk_free + self.vol * self.vol / 2) * self.time) / (self.vol * math.sqrt(self.time))
+            self.d2 = self.d1 - self.vol * math.sqrt(self.time)
+        else:
+            self.d1 = 5
+            
+
 
     def intrinsic(self):
         if self.option_type == "Call":
